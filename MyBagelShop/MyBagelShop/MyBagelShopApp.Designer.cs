e67dedb97d49da1b44e146ee8eddb664e363d0bb
pranalpatil <pppranal@gmail.com>
2022-11-17 16:41:00 +0000

    update

--- MyBagelShop/MyBagelShop/MyBagelShopApp.Designer.cs
+++ MyBagelShop/MyBagelShop/MyBagelShopApp.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyBagelShopApp));
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectBagelgroupBox = new System.Windows.Forms.GroupBox();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.selectBagelgroupBox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(189)))), ((int)(((byte)(207)))));
+            this.topPanel.Controls.Add(this.logoPictureBox);
+            this.topPanel.Location = new System.Drawing.Point(0, -3);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1660, 145);
+            this.topPanel.TabIndex = 0;
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Location = new System.Drawing.Point(0, 3);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(435, 142);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 0;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 116);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // selectBagelgroupBox
+            // 
+            this.selectBagelgroupBox.Controls.Add(this.button1);
+            this.selectBagelgroupBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectBagelgroupBox.Location = new System.Drawing.Point(12, 148);
+            this.selectBagelgroupBox.Name = "selectBagelgroupBox";
+            this.selectBagelgroupBox.Size = new System.Drawing.Size(836, 419);
+            this.selectBagelgroupBox.TabIndex = 14;
+            this.selectBagelgroupBox.TabStop = false;
+            this.selectBagelgroupBox.Text = "Select Bagel";
             // 
             // MyBagelShopApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1513, 1147);
+            this.ClientSize = new System.Drawing.Size(1657, 1147);
+            this.Controls.Add(this.selectBagelgroupBox);
+            this.Controls.Add(this.topPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MyBagelShopApp";
-            this.Text = "Form1";
+            this.Text = "Welcome to MyBagelShop";
+            this.topPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.selectBagelgroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox selectBagelgroupBox;
     }
 }
 
